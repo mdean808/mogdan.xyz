9b16b815c01654edc9c2211830642002d318a639
Morgan Dean <morgandeanhi@gmail.com>
2026-07-21 12:51:52 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,7 @@ export default defineConfig({
       // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
       // See https://svelte.dev/docs/kit/adapters for more information about adapters.
       adapter: adapter({
-        pages: 'docs',
-        assets: 'docs'
+        fallback: '404.html'
       })
     })
   ]
