2e9ad6d249ffa6dc95525d0a86e12b93afb675b9
Morgan Dean <morgandeanhi@gmail.com>
2026-07-21 13:08:50 -0700

    link to gh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     <p class="py-1">I'm Morgan Dean, a software developer based in Vancouver, Canada.</p>
     <p class="py-1">
       I'm currently working on <a href="https://friendsfm.app">Friends FM</a>, an interactive diary
-      of my <a href="https://bikepacking.mogdan.xyz">bikepacking trips</a>, and various
+      of my <a href="https://github.com/mdean808/bikepacking">bikepacking trips</a>, and various
       <router-link to="/projects">programming projects</router-link>.
     </p>
     <p class="py-1">Want more? Check out my <a href="https://github.com/mdean808">GitHub</a>.</p>
